ee9f1c9a67678875b82ad168c05eeffdf58fb4e2
Henry Lee <henryleeworld@gmail.com>
2024-11-05 06:54:31 +0800

    feat:升級至 laravel 11

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,15 +8,15 @@ class UsersController extends Controller
 {
     public function archive() 
     {
-        $user = User::where('id', 1)->firstOrFail();
+        $user = User::where('id', $id = 1)->firstOrFail();
 		$userCount = $user->count();
-        echo (($userCount > 0) ? '使用者編號 1 存在' : '使用者編號 1 不存在') . PHP_EOL;
-        echo '將使用者編號 1 進行封存' . PHP_EOL;
+        echo (($userCount > 0) ? __('User ID :id exists', ['id' => $id]) : __('User ID :id does not exist', ['id' => $id])) . PHP_EOL;
+        echo __('Archive user ID :id', ['id' => $id]) . PHP_EOL;
         $user->archive();
-        $user = User::where('id', 1)->first();
-        echo (!empty($user) ? '使用者編號 1 存在' : '使用者編號 1 不存在') . PHP_EOL;
-        $user = User::where('id', 1)->withArchived()->firstOrFail();
+        $user = User::where('id', $id = 1)->first();
+        echo (!empty($user) ? __('User ID :id exists', ['id' => $id]) : __('User ID :id does not exist', ['id' => $id])) . PHP_EOL;
+        $user = User::where('id', $id = 1)->withArchived()->firstOrFail();
 		$userCount = $user->count();
-        echo '（封存區）' . (($userCount > 0) ? '使用者編號 1 存在' : '使用者編號 1 不存在') . PHP_EOL;
+        echo (__('(Archived area)') . (($userCount > 0) ? __('User ID :id exists', ['id' => $id]) : __('User ID :id does not exist', ['id' => $id]))) . PHP_EOL;
     }
 }
